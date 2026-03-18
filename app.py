f105acb27e4a9980b55aca84cfc60930e9ade661
BezuidenhoutMC <bezmc93@gmail.com>
2026-03-18 09:29:39 +0000

    asdasdasasd

--- app.py
+++ app.py
@@ -11,45 +11,56 @@ st.set_page_config(page_title="SKAO Calibrator Search", layout="wide")
 
 st.title("SKAO Calibrator Search")
 
-# --- File upload ---
-# Load data
-ATCAdb = pd.read_csv('ATCA Calibrators Database.csv')
 
-# st.write("Preview of data:")
-# st.dataframe(ATCAdb.head())
+def ParseATCA(fname):
+    try:
+        ATCAdb = pd.read_csv(fname)
+    except Exception as e:
+        st.error(f"Error reading ATCA Database file: {e}")     
 
-# --- Coordinate conversion ---
-try:
-    ra_vals = Angle(ATCAdb["R.A."], unit=u.hourangle)
-    dec_vals = Angle(ATCAdb["Dec."], unit=u.degree)
+    # --- Coordinate conversion ---
+    try:
+        ra_vals = Angle(ATCAdb["R.A."], unit=u.hourangle)
+        dec_vals = Angle(ATCAdb["Dec."], unit=u.degree)
 
-    c = SkyCoord(ra=ra_vals, dec=dec_vals, frame="icrs")
+        c = SkyCoord(ra=ra_vals, dec=dec_vals, frame="icrs")
 
-    ra_rad = c.ra.wrap_at(180 * u.deg).radian
-    dec_rad = c.dec.radian
+        ra_rad = c.ra.wrap_at(180 * u.deg).radian
+        dec_rad = c.dec.radian
 
-    # # --- Plot ---
-    # fig, ax = plt.subplots(
-    #     figsize=(8, 4.2),
-    #     subplot_kw=dict(projection="mollweide")
-    # )
+        return c,ra_rad,dec_rad
+        # st.success(f"{len(c)} sources before cut")
 
-    # ax.grid(True)
-    # ax.scatter(ra_rad, dec_rad, marker="o", s=2, alpha=0.3)
+    except Exception as e:
+        st.error(f"Error processing ATCA data: {e}")
 
-    # ax.set_title("ATCA calibrators (before cut)")
+def ATCA_cuts(Dec_lim,Flux_lim,ATCAdb):
+    ## Apply Declination & Flux density cuts at 4cm
+    try: 
+        ATCA_CutDec = ATCAdb[Angle(ATCAdb["Dec."],unit=u.deg).deg< Dec_lim]
+        ATCA_CutFlux = ATCA_CutDec[
+            (ATCA_CutDec["4cm"] > Flux_lim) | (ATCA_CutDec["15mm"] > Flux_lim)]
 
-    # # Show in Streamlit
-    # st.pyplot(fig)
+        return ATCA_CutFlux
 
-    # --- Info ---
-    st.success(f"{len(c)} sources before cut")
+    except Exception as e:
+        st.error(f"Error making ATCA cuts {e}")
 
-except Exception as e:
-    st.error(f"Error processing ATACA data: {e}")
+def plotSkyCoords(ra_rad,dec_rad):
+    ## Plot ATCA catalog after cuts
+    try:
+        fig, ax = plt.subplots(figsize=(8, 4.2), subplot_kw=dict(projection="mollweide"))
+        ax.grid(True)
+        ax.scatter(ra_rad, dec_rad, marker="o", s=2, alpha=0.3)
+        fig.subplots_adjust(top=0.95, bottom=0.0)
+        ax.set_title('ATCA Calibrators (after cut)')
+        st.pyplot(fig)
+        st.success(f"{len(ATCA_CutFlux)} sources after Flux & Dec cuts")
+        st.write(ATCA_CutFlux)
+    except Exception as e:
+        st.error(f"Error plotting ATCA after cuts: {e}")
 
-## Apply Declination & Flux density cuts at 4cm
-try: 
+def main():
     st.sidebar.header("Filter settings")
 
     Dec_lim = st.sidebar.number_input(
@@ -58,38 +69,26 @@ try:
     max_value=90.0,
     value=30.0,
     step=1.0
-)
+    )
 
     Flux_lim = st.sidebar.slider(
     "Minimum Flux (Jy)",
     min_value=0.0,
+    max_value = 100.0,
     value=5.0,
     step=0.1
-)
+    )
 
-    ATCA_CutDec = ATCAdb[Angle(ATCAdb["Dec."],unit=u.deg).deg< Dec_lim]
-    ATCA_CutF4cm = ATCA_CutDec[
-        (ATCA_CutDec["4cm"] > Flux_lim) | (ATCA_CutDec["15mm"] > Flux_lim)]
+    ATCAdb = ParseATCA('ATCA Calibrators Database.csv')
+    ATCA_after_cuts = ATCA_cuts(Dec_lim,Flux_lim,ATCAdb)
 
-except Exception as e:
-    st.error(f"Error making ATCA cuts {e}")
-
-## Plot ATCA catalog after cuts
-try:
-    ra_vals = Angle(ATCA_CutF4cm["R.A."], unit=u.hourangle)
-    dec_vals = Angle(ATCA_CutF4cm["Dec."], unit=u.degree)
+    ra_vals = Angle(ATCA_after_cuts["R.A."], unit=u.hourangle)
+    dec_vals = Angle(ATCA_after_cuts["Dec."], unit=u.degree)
 
     c = SkyCoord(ra=ra_vals, dec=dec_vals, frame='icrs')
     ra_rad = c.ra.wrap_at(180 * u.deg).radian
     dec_rad = c.dec.radian
 
-    fig, ax = plt.subplots(figsize=(8, 4.2), subplot_kw=dict(projection="mollweide"))
-    ax.grid(True)
-    ax.scatter(ra_rad, dec_rad, marker="o", s=2, alpha=0.3)
-    fig.subplots_adjust(top=0.95, bottom=0.0)
-    ax.set_title('ATCA Calibrators (after cut)')
-    st.pyplot(fig)
-    st.success(len(ATCA_CutF4cm), 'sources after 4cm & 16cm Flux & Dec cuts')
-    st.write(ATCA_CutF4cm)
-except Exception as e:
-    st.error(f"Error plotting ATCA after cuts: {e}")
+    plotSkyCoords(ra_rad,dec_rad)
+
+main()
