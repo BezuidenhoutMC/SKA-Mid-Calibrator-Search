3fd221ac1d3f416bd2ae2870ebcab06bd2a5979a
BezuidenhoutMC <bezmc93@gmail.com>
2026-03-30 09:14:02 +0000

    a

--- app.py
+++ app.py
@@ -748,14 +748,7 @@ def conesearch_PMN(calibrators):
             sorted_seps_deg = seps_deg[order]
 
             for k, sep_deg in zip(sorted_idxs, sorted_seps_deg):
-                try:
-                    if PMN_cat_gflux_scaled[k]:
-                        PMN_flux = PMN_cat_gflux_scaled[k]
-                    else:
-                        PMN_flux = PMN_cat_flux_scaled[k]
-                except:
-                    print('**************************************************')
-                    continue
+                PMN_flux = PMN_cat_gflux_scaled[k] if PMN_cat_gflux_scaled[k] else PMN_cat_flux_scaled[k]
 
                 if np.isclose(sep_deg, 0.0, atol=1e-2):
                     continue
